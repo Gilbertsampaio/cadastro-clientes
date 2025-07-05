2883c07341ae85835e94af92e6c9fc5351aae004
GilbertSampaio <sampaiogilbert@gmail.com>
2025-07-04 22:18:37 -0300

    Criação das rotas
    Signed-off-by: GilbertSampaio <sampaiogilbert@gmail.com>

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/clientes', 'Clientes::index');
+$routes->get('/clientes/criar', 'Clientes::criar');
+$routes->post('/clientes/salvar', 'Clientes::salvar');
+$routes->get('/clientes/editar/(:num)', 'Clientes::editar/$1');
+$routes->post('/clientes/atualizar/(:num)', 'Clientes::atualizar/$1');
+$routes->get('/clientes/excluir/(:num)', 'Clientes::excluir/$1');
